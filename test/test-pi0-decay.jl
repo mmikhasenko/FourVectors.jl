cfc7be9b40d2ba7615c2a456388a90201e6afc54
Mikhail Mikhasenko <mikhail.mikhasenko@cern.ch>
2024-10-05 19:01:47 +0200

    no unpacking should work like this on julia 1.3

--- test/test-pi0-decay.jl
+++ test/test-pi0-decay.jl
@@ -6,10 +6,10 @@ pπ0 = FourVector(1.0, 1.0, 30.0; M = mπ0)
 #
 pγ1 = mπ0 / 2 .* FourVector(sin(0.3), 0.0, cos(0.3); E = 1.0)
 pγ1 = Bz(pγ1, boost_gamma(pπ0))
-(; cosθ, ϕ) = spherical_coordinates(pπ0)
+Ω = spherical_coordinates(pπ0)
 
-pγ1 = Ry(pγ1, acos(cosθ))
-pγ1 = Rz(pγ1, ϕ)
+pγ1 = Ry(pγ1, acos(Ω.cosθ))
+pγ1 = Rz(pγ1, Ω.ϕ)
 #
 pγ2 = pπ0 - pγ1
 #
